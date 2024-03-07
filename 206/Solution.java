7abedf3ba0db4c33634e1bbdd722cd4258d7287d
vivksingh <vivksing655@gmail.com>
2024-03-07 15:56:26 +0530

    reverse linkdlist

---
+++ 206/Solution.java
@@ -0,0 +1,36 @@
+class Solution {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode reverseList(ListNode head) {
+        if (head == null)
+            return head;
+        ListNode prev = null;
+        ListNode cur = head;
+        ListNode next = null;
+
+        while (cur != null) {
+            next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+
+        return prev;
+    }
+}
